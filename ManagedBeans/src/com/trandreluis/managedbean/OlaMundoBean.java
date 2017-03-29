506ff21f4d64ff71bb8d1fa539eb6792bada6aa0
Andre Luis <tr.andreluis@gmail.com>
2017-03-29 02:55:14 -0300

    Criacao do bean gerenciavel OlaMundoBean

---
+++ ManagedBeans/src/com/trandreluis/managedbean/OlaMundoBean.java
@@ -0,0 +1,18 @@
+package com.trandreluis.managedbean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class OlaMundoBean {
+
+	private String nome;
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
