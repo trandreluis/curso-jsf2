56dbfe140faaeb6162e29e6acd60fb34c81142d6
Andre Luis <tr.andreluis@gmail.com>
2017-03-30 18:53:39 -0300

    Criacao do projeto para aprendizado de escopos de aplicacao e sessao

---
+++ EscoposAplicacaoSessao/src/com/trandreluis/visao/GestaoProdutosBean.java
@@ -0,0 +1,16 @@
+package com.trandreluis.visao;
+
+import javax.faces.bean.ManagedBean;
+
+import com.trandreluis.dominio.Produto;
+
+@ManagedBean
+public class GestaoProdutosBean {
+
+	public void incluir(Produto produto) {
+		
+		
+		
+	}
+	
+}
