fd14db57acd01c8c67502c639a18355fd7ee3605
Andre Luis <tr.andreluis@gmail.com>
2017-03-31 01:10:57 -0300

    Implementacao da listagem de dados

--- EscoposAplicacaoSessao/src/com/trandreluis/visao/GestaoProdutosBean.java
+++ EscoposAplicacaoSessao/src/com/trandreluis/visao/GestaoProdutosBean.java
@@ -1,16 +1,35 @@
 package com.trandreluis.visao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
 import com.trandreluis.dominio.Produto;
 
 @ManagedBean
 public class GestaoProdutosBean {
 
-	public void incluir(Produto produto) {
-		
-		
-		
+	private List<Produto> produtos;
+	private Produto produto;
+	
+	public GestaoProdutosBean() {
+		this.produtos = new ArrayList<Produto>();
+		this.produto = new Produto();
+	}
+	
+	public void incluir() {
+		this.produtos.add(this.produto);
+		this.produto = new Produto();
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
 	}
 	
 }
