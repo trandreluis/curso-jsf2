ac1687dd9494af4ea3036947c984961f9f01161f
Andre Luis <tr.andreluis@gmail.com>
2017-03-31 01:58:18 -0300

    Criacao do projeto para se estudar outros escopos de manageds beans

---
+++ OutrosEscoposManagedBeans/src/com/trandreluis/visao/GestaoProdutosBean.java
@@ -0,0 +1,36 @@
+package com.trandreluis.visao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import com.trandreluis.dominio.Produto;
+
+@ManagedBean
+@SessionScoped
+public class GestaoProdutosBean {
+
+	private List<Produto> produtos;
+	private Produto produto;
+	
+	public GestaoProdutosBean() {
+		this.produtos = new ArrayList<Produto>();
+		this.produto = new Produto();
+	}
+	
+	public void incluir() {
+		this.produtos.add(this.produto);
+		this.produto = new Produto();
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+	
+}
