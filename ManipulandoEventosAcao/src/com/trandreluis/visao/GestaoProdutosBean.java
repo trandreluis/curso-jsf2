4ac758be68b42c4faae3839a67e7ebb49abc88df
Andre Luis <tr.andreluis@gmail.com>
2017-04-01 04:11:52 -0300

    Criando projeto peara tabalhar com eventos de acao

---
+++ ManipulandoEventosAcao/src/com/trandreluis/visao/GestaoProdutosBean.java
@@ -0,0 +1,45 @@
+package com.trandreluis.visao;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import com.trandreluis.dominio.Produto;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@SessionScoped
+public class GestaoProdutosBean implements Serializable {
+
+	private List<Produto> produtos;
+	private Produto produto;
+	
+	public GestaoProdutosBean() {
+		this.produtos = new ArrayList<Produto>();
+		this.produto = new Produto();
+	}
+	
+	public void incluir() {
+		this.produtos.add(this.produto);
+		this.produto = new Produto();
+	}
+
+	public String obterAjuda() {
+		if(this.produtos.isEmpty()) {
+			return "AjudaGestaoProdutosTelefone?faces-redirect=true";
+		}
+		return "AjudaGestaoProdutos?faces-redirect=true";
+	}
+	
+	public Produto getProduto() {
+		return produto;
+	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+	
+}
