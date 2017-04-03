a4c919e03a32b2eab3aba79530c827589adabc91
Andre Luis <tr.andreluis@gmail.com>
2017-04-03 19:26:39 -0300

    Cricao do projeto para manipulação de eventos de mudança de valor

---
+++ ManipulandoEventosMudancaValor/src/com/trandreluis/visao/GestaoProdutosBean.java
@@ -0,0 +1,70 @@
+package com.trandreluis.visao;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.event.ActionEvent;
+
+import com.trandreluis.dominio.Produto;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@SessionScoped
+public class GestaoProdutosBean implements Serializable {
+
+	private List<Produto> produtos;
+	private Produto produto;
+	
+	private Produto produtoSelecionado;
+	
+	public Produto getProdutoSelecionado() {
+		return produtoSelecionado;
+	}
+
+	public void setProdutoSelecionado(Produto produtoSelecionado) {
+		this.produtoSelecionado = produtoSelecionado;
+	}
+
+	public GestaoProdutosBean() {
+		this.produtos = new ArrayList<Produto>();
+		this.produto = new Produto();
+	}
+	
+	public void incluir() {
+		this.produtos.add(this.produto);
+		this.produto = new Produto();
+	}
+
+	public String obterAjuda() {
+		if(this.produtos.isEmpty()) {
+			return "AjudaGestaoProdutosTelefone?faces-redirect=true";
+		}
+		return "AjudaGestaoProdutos?faces-redirect=true";
+	}
+
+	public void verificarInclusao(ActionEvent evento) {
+		if("".equals(this.produto.getFabricante())) {
+			this.produto.setFabricante("Sem Frabricante");
+		}
+		
+		if("".equals(this.produto.getCategoria())) {
+			this.produto.setCategoria("Sem Categoria");
+		}
+	}
+	
+	public void excluir() {
+		this.produtos.remove(this.produtoSelecionado);
+	}
+	
+	public Produto getProduto() {
+		return produto;
+	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+	
+}
