45733950c8d10ef95f98ca0dc2568e3a4d48de57
Andre Luis <tr.andreluis@gmail.com>
2017-04-04 05:14:48 -0300

    Correcao na busca

--- ManipulandoEventosMudancaValor/src/com/trandreluis/visao/GestaoProdutosBean.java
+++ ManipulandoEventosMudancaValor/src/com/trandreluis/visao/GestaoProdutosBean.java
@@ -21,56 +21,56 @@ public class GestaoProdutosBean implements Serializable {
 	private List<Produto> produtos;
 	private List<Produto> produtosFiltrados;
 	private Produto produto;
-	
+
 	private Produto produtoSelecionado;
-	
+
 	public GestaoProdutosBean() {
 		this.produtos = new ArrayList<Produto>();
 		this.produtosFiltrados = new ArrayList<Produto>();
 		this.produto = new Produto();
 	}
-	
+
 	public void nomePesquisaAlterado(ValueChangeEvent evento) {
-		System.out.println("Valor atual: "+this.nomePesquisa);
-		System.out.println("Valor novo: "+evento.getNewValue());
-		
+		System.out.println("Valor atual: " + this.nomePesquisa);
+		System.out.println("Valor novo: " + evento.getNewValue());
+
 		this.produtosFiltrados.clear();
-		
-		for(Produto produto : this.produtos) {
-			if(produto.getNome().startsWith((String)evento.getNewValue())) {
+
+		for (Produto produto : this.produtos) {
+			if (produto.getNome().toLowerCase().startsWith(evento.getNewValue().toString().toLowerCase())) {
 				this.produtosFiltrados.add(produto);
 			}
 		}
-		
+
 	}
-	
+
 	public void incluir() {
 		this.produtos.add(this.produto);
 		this.produto = new Produto();
 	}
 
 	public void verificarInclusao(ActionEvent evento) {
-		if("".equals(this.produto.getFabricante())) {
+		if ("".equals(this.produto.getFabricante())) {
 			this.produto.setFabricante("Sem Frabricante");
 		}
-		
-		if("".equals(this.produto.getCategoria())) {
+
+		if ("".equals(this.produto.getCategoria())) {
 			this.produto.setCategoria("Sem Categoria");
 		}
 	}
-	
+
 	public void excluir() {
 		this.produtos.remove(this.produtoSelecionado);
 	}
-	
+
 	public Produto getProduto() {
 		return produto;
 	}
-	
+
 	public List<Produto> getProdutos() {
 		return produtos;
 	}
-	
+
 	public Produto getProdutoSelecionado() {
 		return produtoSelecionado;
 	}
@@ -90,5 +90,5 @@ public class GestaoProdutosBean implements Serializable {
 	public List<Produto> getProdutosFiltrados() {
 		return produtosFiltrados;
 	}
-	
+
 }
