07f2d87b2b4fd2663b810820f086813a2d7a8612
Andre Luis <tr.andreluis@gmail.com>
2017-04-18 20:48:06 -0300

    Implementacao da verificação do dia da semana

--- Mensagens/src/com/trandreluis/bean/CadastroUsuarioBean.java
+++ Mensagens/src/com/trandreluis/bean/CadastroUsuarioBean.java
@@ -1,5 +1,6 @@
 package com.trandreluis.bean;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import javax.faces.application.FacesMessage;
@@ -18,25 +19,36 @@ public class CadastroUsuarioBean {
 	public void cadastrar() {
 
 		if (this.getNome() != null || this.getNome().length() < 10) {
-			this.adicionarMenasagem("frm:nome", FacesMessage.SEVERITY_ERROR, "Informe o nome corretamente",
+			this.adicionarMenasagem("frm:nome", FacesMessage.SEVERITY_ERROR, "Informe o nome corretamente.",
 					"O nome deve conter mais de 10 caracters!");
 		}
-		
+
+		if (diaDeDescanso()) {
+			this.adicionarMenasagem(null, FacesMessage.SEVERITY_WARN, "Hoje é domingo",
+					"Domingo é um dia de descanso.");
+		}
+
 	}
 
 	/**
 	 * 
 	 * @param componentID
-	 *            tambem conhecido como clienteID
+	 *            tambem conhecido como clientID
 	 */
 	private void adicionarMenasagem(String componentID, Severity severidade, String sumarioErro, String detalheErro) {
 
 	}
 
 	public boolean diaDeDescanso() {
+		
+		if(Calendar.getInstance().get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
+			return true;			
+		}
+		
 		return false;
+		
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
